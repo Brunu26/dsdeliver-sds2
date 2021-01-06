54b920103362a78ba1bbfa313bc8078bbb137801
Bruno <bruno03araujo@hotmail.com>
2021-01-06 13:22:28 -0300

    Front end web parte 1

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,28 @@
 import './styles.css';
 import {ReactComponent as Logo} from './logo.svg';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect , useState} from 'react';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+import { isConstructorDeclaration } from 'typescript';
 function Orders(){
+
+    const [products, setProducts] =  useState<Product[]>([]);
+
+    
+
+    useEffect(() =>{
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    },[]);
+
     return (
         <div className="orders-container">
             <StepsHeader />
-        
+            <ProductsList products={products} />
+
 
         </div>
 
